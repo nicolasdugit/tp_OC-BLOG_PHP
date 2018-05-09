6c0c992fadbbda5726dcf133145cc21e7c343fa4
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-09 14:47:46 +0700

    code d'ajout commentaire en bdd

--- commentaires_post.php
+++ commentaires_post.php
@@ -1,4 +1,14 @@
 <?php
-
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=testOC;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+	die('Erreur : ' .$e->getMessage());
+}
+$req = $bdd->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, date_commentaire VALUES (? ,? ,? ,NOW())) ');
+$req->execute(array($_POST['billet'],$_POST['auteur'],$_POST['commentaire']));
+//redirection vers commentaires.php
 header('location: commentaires.php');
 ?>
