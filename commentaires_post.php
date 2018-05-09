fae0ebadf4acaa58ffb0f3c5144785acf51682fc
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-09 15:59:12 +0700

    rajout verif faille xss

--- commentaires_post.php
+++ commentaires_post.php
@@ -8,7 +8,7 @@ catch (Exception $e)
 	die('Erreur : ' .$e->getMessage());
 }
 $req = $bdd->prepare('INSERT INTO commentaires (id_billet, auteur, commentaire, date_commentaire) VALUES (?,?,?, NOW() )');
-$req->execute(array($_GET['billet'],$_POST['auteur'],$_POST['commentaire']));
+$req->execute(array($_GET['billet'], htmlspecialchars($_POST['auteur']), htmlspecialchars($_POST['commentaire'])));
 //redirection vers commentaires.php
 $redirect = $_GET['billet'];
 header("location: commentaires.php?billet=$redirect");
