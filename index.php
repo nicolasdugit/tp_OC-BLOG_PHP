e386f50c59efe619d711e9a123258faf7d93ce1e
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-09 11:22:40 +0700

    balise head

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Mon super blog !</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<h1>Mon super Blog !</h1>
+</body>
+</html>
