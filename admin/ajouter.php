fb60db5ab69528ccf89a311afbd4e9a5c891d135
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-10 15:31:29 +0700

    creation dossier admin protege par mdp

---
+++ admin/ajouter.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ajouter article</title>
+</head>
+<body>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 	<h1>Mon super Blog !</h1>
+	<a href="admin/ajouter.php">admin</a>
 	<p>Derniers billets du blog : </p>
 	<?php 
 	try
