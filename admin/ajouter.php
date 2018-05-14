730b6f90d55bb352e7753ca2ccc79139936eed8e
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-14 14:07:32 +0700

    installation tinymce

--- admin/ajouter.php
+++ admin/ajouter.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ajouter article</title>
+	<script src="../node_modules/tinymce/tinymce.min.js"></script>
+  	<script>tinymce.init({ selector:'#tunymce' });</script>
+  	<title>Ajouter article</title>
 </head>
 <body>
 	<form action="ajouter_post.php" method="POST">
 		<label for="titre">Titre du message : </label> <input type="text" name="titre">
-		<label for="contenu">Message : </label> <textarea name="contenu"></textarea>
+		<label for="contenu">Message : </label> <textarea id="tinymce" name="contenu"></textarea>
 		<input type="submit">
 	</form>
 	<a href="administrateur.php">Retour page admin</a>
